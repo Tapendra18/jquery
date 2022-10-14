d80a848b31ac75510c51242298ece98a62734a7b
Tapendra18 <107476177+Tapendra18@users.noreply.github.com>
2022-10-14 17:45:38 +0530

    subheading button

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 
 
-//   $(document).ready(function() {
-//     $(".formsubmit").submit(function() {
-//       alert($("header",this).val());
-//     });
-//   })
 
-//   $(".fromsubmit").click(function (e) {
-    
-//     e.preventDefault();
-// });
 $(document).ready(function(){
     $('.heading').submit(function(event){
         event.preventDefault();
        var heading =  $(".header").val();
        console.log(heading);
-       $("main").append("<section><h3>"+heading+"</h3></section>");
+       $("main").append("<section><h1>"+heading+"</h1></section>");
         $("#exampleModal1").modal('toggle');
-    
+        $(".SubHeadingValue").append("<option><h1>"+heading+"</h1></option>");
+        
+        // $("#exampleModal1").modal('toggle');
     })
 })
 
-
+$(document).ready(function(){
+    $('.SubHeading').submit(function(event){
+        event.preventDefault();
+        var subheading = $('.subheadingval').val();
+        console.log(subheading);
+        $("main").append("<div><h3>"+subheading+"</h3></div>");   
+    })
+})
 
 // $(".heading").submit(function(event){
 //        event.preventDefault();
